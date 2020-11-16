84b1b67f3028fdb560b52156f4417fe7a41188e4
Andy Luhrs <aluhrs@outlook.com>
2020-11-16 14:05:58 -0800

    Update Logo.js

--- src/components/ui/Logo.js
+++ src/components/ui/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../Logo.svg";
+import logo from "../../logo.svg";
 
 export default function Logo(props) {
     return <img src={logo} alt="logo" {...props} />;
